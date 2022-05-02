a8db4ebf347f63e82e91b58454ba4d8d0afca0f3
wasimreza <wasim.cse.2k8@gmail.com>
2022-05-02 13:28:10 +0200

    add adapter delegate ext and app

--- core/src/main/java/com/example/core/dispatcher/MainDispatcherProvider.kt
+++ core/src/main/java/com/example/core/dispatcher/MainDispatcherProvider.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
-class MainDispatcherProvider @Inject constructor(): BaseDispatcherProvider {
+class MainDispatcherProvider @Inject constructor() : BaseDispatcherProvider {
     override fun main(): CoroutineDispatcher = Dispatchers.Main
     override fun io(): CoroutineDispatcher = Dispatchers.IO
     override fun compute(): CoroutineDispatcher = Dispatchers.Default
