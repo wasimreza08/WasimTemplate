87f6015ec929a67e2d236016211128594bbea046
wasimreza <wasim.cse.2k8@gmail.com>
2022-08-27 23:51:29 +0200

    abstract module added

--- app/src/main/java/com/example/wasim/MainActivity.kt
+++ app/src/main/java/com/example/wasim/MainActivity.kt
@@ -6,10 +6,12 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    // todo add feature launcher
+    // @Inject
+    // lateinit var launcher: TestLauncher
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+       // launcher.launch(this)
     }
-
 }
