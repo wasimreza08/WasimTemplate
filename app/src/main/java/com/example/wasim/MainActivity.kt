9849c7bba3123a8d39f3190acd40c371a4dbcf35
wasimreza <wasim.cse.2k8@gmail.com>
2022-11-29 21:39:18 +0100

    update template

--- app/src/main/java/com/example/wasim/MainActivity.kt
+++ app/src/main/java/com/example/wasim/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.wasim
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -13,25 +13,17 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     // todo add feature launcher
     // @Inject
-    // lateinit var launcher: TestRunLauncher
-    private val activityScope = CoroutineScope(Dispatchers.Main)
+    // lateinit var launcher: LabLauncher
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        activityScope.launch {
-            delay(SPLASH_SCREEN_TIME)
-            // launcher.launch(this@MainActivity)
-            finish()
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                delay(timeMillis = 2000)
+                //launcher.launch(this@MainActivity)
+                finish()
+            }
         }
     }
-
-    override fun onDestroy() {
-        activityScope.cancel()
-        super.onDestroy()
-    }
-
-    companion object {
-        private const val SPLASH_SCREEN_TIME = 2000L
-    }
-
 }
--- core/src/main/java/com/example/core/viewmodel/BaseViewModel.kt
+++ core/src/main/java/com/example/core/viewmodel/BaseViewModel.kt
@@ -2,23 +2,16 @@ package com.example.core.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
-abstract class BaseViewModel<UiEvent : ViewEvent, UiState : ViewState, UiEffect : ViewEffect> :
-    ViewModel() {
+abstract class BaseViewModel<UiEvent : ViewEvent, UiState : ViewState> : ViewModel() {
 
     private val initialState: UiState by lazy { provideInitialState() }
     private val _viewState: MutableStateFlow<UiState> by lazy { MutableStateFlow(initialState) }
     val viewState: StateFlow<UiState> by lazy { _viewState }
-
-    // Effect (side effects like error messages which we want to show only once)
-    private val _effect: Channel<UiEffect> = Channel()
-    val effect = _effect.receiveAsFlow()
 
     // Event (user actions)
     private val _event: MutableSharedFlow<UiEvent> = MutableSharedFlow()
@@ -38,19 +31,13 @@ abstract class BaseViewModel<UiEvent : ViewEvent, UiState : ViewState, UiEffect 
         _viewState.value = newState
     }
 
-    fun onUiEvent(event: UiEvent) {
+    fun onEvent(event: UiEvent) {
         viewModelScope.launch { _event.emit(event) }
     }
 
     abstract fun handleEvent(event: UiEvent)
-
-    protected fun sendEffect(effect: UiEffect) {
-        viewModelScope.launch { _effect.send(effect) }
-    }
 }
 
 interface ViewState
 
 interface ViewEvent
-
-interface ViewEffect
